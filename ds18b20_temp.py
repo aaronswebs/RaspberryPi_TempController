d9a4c8b84d8ce8f6539fa026a0a05f8522244099
Aaron Morrison <aamorris@microsoft.com>
2020-08-09 12:47:58 +1000

    fixed missing arg in read_temp function

--- ds18b20_temp.py
+++ ds18b20_temp.py
@@ -28,7 +28,7 @@ def read_temp(device_file):
     lines = read_temp_raw(device_file)
     while lines[0].strip()[-3:] != 'YES':
         time.sleep(0.2)
-        lines = read_temp_raw()
+        lines = read_temp_raw(device_file)
     equals_pos = lines[1].find('t=')
     if equals_pos != -1:
         temp_string = lines[1][equals_pos+2:]
