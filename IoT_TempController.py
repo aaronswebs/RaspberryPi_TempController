5c0e2002235c0bb54680b29a2a403b3eb7ce168a
Aaron Morrison <aamorris@microsoft.com>
2020-08-13 13:20:56 +1000

    updated lcd display function

--- IoT_TempController.py
+++ IoT_TempController.py
@@ -32,14 +32,6 @@ lcd.color = [100, 0, 0]
 LoTempDS18B20 = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, sensorConstant.normaltempSensor)
 HiTempDS18B20 = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, sensorConstant.hightempSensor)
 
-# The device connection string to authenticate the device with your IoT hub.
-# Using the Azure CLI:
-# az iot hub device-identity show-connection-string –hub-name {YourIoTHubName} –device-id MyNodeDevice –output table
-CONNECTION_STRING = "HostName=iotbrew.azure-devices.net;DeviceId=brew-rpi;SharedAccessKey=QWAixdM8EYoBMDyUL7kqcXfA/Tddx6oE62s6KERGNJI="
-
-# Define the JSON message to send to IoT Hub.
-MSG_TXT = "{\"temperature\": %.2f,\"pressure\": %.2f,\"humidity\": %.2f,\"sensor1_temp\": %.2f,\"sensor2_temp\": %.2f,\"dewpoint\": %.2f}"
-
 # define degrees sign character
 degrees_symbol = u'\N{DEGREE SIGN}'
 lcd_degrees = chr(223)
@@ -86,45 +78,53 @@ def set_mean_sea_level_pressure():
   bme280.sea_level_pressure = meanSeaLevelPressure
 
 def iothub_client_init():
-    # Create an IoT Hub client
-    client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
-    return client
+  # The device connection string to authenticate the device with your IoT hub.
+  # Using the Azure CLI:
+  # az iot hub device-identity show-connection-string –hub-name {YourIoTHubName} –device-id MyNodeDevice –output table
+  CONNECTION_STRING = "HostName=iotbrew.azure-devices.net;DeviceId=brew-rpi;SharedAccessKey=QWAixdM8EYoBMDyUL7kqcXfA/Tddx6oE62s6KERGNJI="
+  # Create an IoT Hub client
+  client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
+  return client
     
 def iothub_client_telemetry_run():
-    client = iothub_client_init()
-    print ( "IoT Hub device sending periodic messages, press Ctrl-C to exit" )
-    try:
-        while True:
-            sensor.get_values()
-            # Build the message with telemetry values.
-            msg_txt_formatted = MSG_TXT % (sensor.ambientTemp, sensor.pressure, sensor.humidity, sensor.LoTempDS18B20, sensor.HiTempDS18B20, sensor.dewpoint)
-            message = Message(msg_txt_formatted)
+  # Define the JSON message to send to IoT Hub.
+  MSG_TXT = "{\"temperature\": %.2f,\"pressure\": %.2f,\"humidity\": %.2f,\"sensor1_temp\": %.2f,\"sensor2_temp\": %.2f,\"dewpoint\": %.2f,\"datetime\": %s}"
+  
+  client = iothub_client_init()
+  print ( "IoT Hub device sending periodic messages, press Ctrl-C to exit" )
+  try:
+      while True:
+          sensor.get_values()
+          # Build the message with telemetry values.
+          msg_txt_formatted = MSG_TXT % (sensor.ambientTemp, sensor.pressure, sensor.humidity, sensor.LoTempDS18B20, sensor.HiTempDS18B20, sensor.dewpoint, datetime.datetime.now())
+          message = Message(msg_txt_formatted)
 
-            # Add a custom application property to the message.
-            # An IoT hub can filter on these properties without access to the message body.
-            if sensor.ambientTemp < 17:
-              message.custom_properties["temperatureAlert"] = "true"
-            else:
-              message.custom_properties["temperatureAlert"] = "false"
-            # Send the message.
-            print ( "Sending message: {}".format(message) )
-            client.send_message(message)
-            print ( "Message successfully sent" )
-            time.sleep(300)
-    except KeyboardInterrupt:
-        print ("\nStopped IoT Messages and Device")
+          # Add a custom application property to the message.
+          # An IoT hub can filter on these properties without access to the message body.
+          if sensor.ambientTemp < 17:
+            message.custom_properties["temperatureAlert"] = "true"
+          else:
+            message.custom_properties["temperatureAlert"] = "false"
+          # Send the message.
+          print ( "Sending message: {}".format(message) )
+          client.send_message(message)
+          print ( "Message successfully sent" )
+          time.sleep(300)
+  except KeyboardInterrupt:
+      print ("\nStopped IoT Messages and Device")
 
 def print_sensor_values():
   while True:
     #lcd is getting sensor values every two seconds.
-    #sensor.get_values()  
-    print ("\nTemperature: %0.1f%sC" % (sensor.ambientTemp, degrees_symbol))
-    print ("LoTemp: %0.1f%sC" % (sensor.LoTempDS18B20, degrees_symbol))
-    print ("HiTemp: %0.1f%sC" % (sensor.HiTempDS18B20, degrees_symbol))
-    print ("Humidity: %0.1f%%" % sensor.humidity)
-    print ("Pressure: %0.1f hPa" % sensor.pressure)
-    print ("Altitude = %0.2f meters" % sensor.altitude)
-    print ("Dewpoint: %0.1f%sC" % (sensor.dewpoint, degrees_symbol))
+    #sensor.get_values() 
+    print ("\nTime: %s" % datetime.datetime.now()) 
+    print ("Temperature: %0.1f%sC" % (sensor.ambientTemp, degrees_symbol))
+    print ("LoTemp:      %0.1f%sC" % (sensor.LoTempDS18B20, degrees_symbol))
+    print ("HiTemp:      %0.1f%sC" % (sensor.HiTempDS18B20, degrees_symbol))
+    print ("Humidity:    %0.1f%%" % sensor.humidity)
+    print ("Pressure:    %0.1f hPa" % sensor.pressure)
+    print ("Altitude:    %0.2f meters" % sensor.altitude)
+    print ("Dewpoint:    %0.1f%sC" % (sensor.dewpoint, degrees_symbol))
     time.sleep(5)
             
 def set_lcd_color(temperature):
@@ -135,38 +135,46 @@ def set_lcd_color(temperature):
   else:
     lcd.color = [100,0,0]
 
+def scroll_lcd_text(lengthOfMessage, time):
+  speed = (lengthOfMessage-16)/time/2
+  for i in range(lengthOfMessage-16):
+    lcd.move_left()
+    time.sleep(speed)
+  for i in range(lengthOfMessage-16):
+    lcd.move_right()
+    time.sleep(speed)
+
+def write_lcd_message(line1, line2, msgDisplayTime):
+  lcd.cursor_position(0,0)
+  lcd.message = line1
+  lcd.cursor_position(0,1)
+  lcd.message = line2
+  if len(line1) > len(line2):
+    msgLength = len(line1)
+  else:
+    msgLength = len(line2)
+  scroll_lcd_text(msgLength,msgDisplayTime)
+  #time.sleep(textDelay)
+
 def write_lcd():
   # put values on LCD
+  msgDisplayTime = 2
   while True:
     sensor.get_values()
     set_lcd_color(sensor.ambientTemp)
-    lcdAmbient = "Temp: %0.1f%sC" % (sensor.ambientTemp, lcd_degrees)
-    lcdPressure = "Pressure: %0.1fhpa" % (sensor.pressure)
-    lcd.cursor_position(0,0)
-    lcd.message = lcdAmbient
-    lcd.cursor_position(0,1)
-    lcd.message = lcdPressure
-    time.sleep(2)
+    line1 = "Temp: %0.1f%sC" % (sensor.ambientTemp, lcd_degrees)
+    line2 = "Pressure: %0.1fhPa" % (sensor.pressure)
+    write_lcd_message(line1, line2, msgDisplayTime)
+    
     sensor.get_values()
-    lcdLoTemp = "LoTemp: %0.1f%sC" % (sensor.LoTempDS18B20, lcd_degrees)
-    lcdHiTemp = "HiTemp: %0.1f%sC" % (sensor.HiTempDS18B20, lcd_degrees)
-    lcd.cursor_position(0,0)
-    lcd.message = lcdLoTemp
-    lcd.cursor_position(0,1)
-    lcd.message = lcdHiTemp
-    time.sleep(2)
+    line1 = "LoTemp: %0.1f%sC" % (sensor.LoTempDS18B20, lcd_degrees)
+    line2 = "HiTemp: %0.1f%sC" % (sensor.HiTempDS18B20, lcd_degrees)
+    write_lcd_message(line1, line2, msgDisplayTime)
+    
     sensor.get_values()
-    lcdHumid = "Humidity: %0.1f%%" % (sensor.humidity)
-    lcdDew = "Dew Point: %0.1f%sC" % (sensor.dewpoint, lcd_degrees)
-    lcd.cursor_position(0,0)
-    lcd.message = lcdHumid
-    lcd.cursor_position(0,1)
-    lcd.message = lcdDew
-    time.sleep(2)
-
-  # for i in range(len(lcd.message)+16):
-  #   lcd.move_right()
-  #   time.sleep(0.5)
+    line1 = "Humidity: %0.1f%%" % (sensor.humidity)
+    line2 = "Dew Point: %0.1f%sC" % (sensor.dewpoint, lcd_degrees)
+    write_lcd_message(line1, line2, msgDisplayTime)
 
 if __name__ == '__main__':
     print ( "IoT Hub client started" )
